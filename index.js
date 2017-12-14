e046b787249bfebb856c3b997697bb07ab129fdc
Rosie Wang <rosiedwang@yahoo.com>
2017-12-14 21:23:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function add(a,b)
   var n = a+b 
   return n
 }
+
+function subtract(a,b)
+{
+  var n = a-b 
+  return n
+}
+
+function multiply(a,b)
+{
+  var n = a*b 
+  return n
+}
+
+function divide(a,b)
+{
+  var n = a/b 
+  return n
+}
+
