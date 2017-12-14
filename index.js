0f7597dd62870a10144366380ba2921cecf3ab65
Rosie Wang <rosiedwang@yahoo.com>
2017-12-14 21:27:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,3 +32,24 @@ function inc(n)
   return ++n
 }
 
+function dec(n)
+{
+  return --n
+}
+
+function makeInt(n)
+{
+  return parseInt(n,10)
+}
+
+
+function preserveDecimal(n)
+{
+  return parseFloat(n)
+}
+
+function inc(n)
+{
+  return ++n
+}
+
